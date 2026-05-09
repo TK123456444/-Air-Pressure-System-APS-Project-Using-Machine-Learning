e549500d6b1be1afbff60023eb8f0fdda74accea
TK123 <tk123@gmail.com>
2026-05-09 12:25:31 +0530

    second  commit

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages,setup
 from typing import List
-
+def requirement_list()->list[str]:
+    requirement_list: list[str] = []
+    return requirement_list
+    pass
 setup(
     name="sensor",
     version="0.0.1",
     author="Tushar",
     author_email="tukumawat73@gmail.com",
     packages=find_packages(),
-    install_requires= ['pymongo']
+    install_requires= requirement_list()
 
 )
